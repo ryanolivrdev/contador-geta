ae6f4062d3ca1c6bedbb08da4e19bc8550e1e8c4
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-03-24 02:25:56 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const buttonSubtrair1 = document.querySelector('.buttonSubtrair1')
+const buttonSubtrair10 = document.querySelector('.buttonSubtrair10')
+const buttonAdicionar1 = document.querySelector('.buttonAdicionar1')
+const buttonAdicionar10 = document.querySelector('.buttonAdicionar10')
+const counter = document.querySelector('.counter')
+
+function check(){
+  switch(){
+  case "+":
+
+  }
+}
+
+function add(){
+  counter.innerText = Number(counter.innerText) + 1
+}
+
+function withdraw(){
+  counter.innerText = Number(counter.innerText) + 1
+}
